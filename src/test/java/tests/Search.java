ac89ac0a4b251be35b56593d541f74ad0f477ff4
AntonDavydov <daver1107@yahoo.com>
2021-10-20 21:16:21 -0700

    Simplified locator methods refactoring

--- src/test/java/tests/Search.java
+++ src/test/java/tests/Search.java
@@ -7,37 +7,46 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Search extends Init {
+//    WebDriverWait wait = new WebDriverWait(driver, 5);
+    private static By search = new By.ByXPath("//*[contains(@text, 'Search')]");
+
 
     @Test
     public void findRandomText() throws Exception {
-        PageObjectMain pom = new PageObjectMain(this.driver);
 
-//        WebElement skip = pom.waitForElementAndClick(
+        PageObjectMain pom = new PageObjectMain(this.driver);
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        driver.findElement(By.id("org.wikipedia:id/fragment_onboarding_skip_button")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(search)).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(search))
+                .sendKeys("asfdasfdasfdasfasfdsadasdas");
+        if (checkSearchResults())
+            System.out.println("Works as expected");
+        else throw new Exception("Actual and expected results do not meet");
+
+        //        WebElement skip = pom.waitForElementAndClick(
 //                "id:org.wikipedia:id/fragment_onboarding_skip_button",
 //                "Cannot find Skip button",
 //                5
 //        );
-        driver.findElement(By.id("org.wikipedia:id/fragment_onboarding_skip_button")).click();
-        driver.findElement(By.id("org.wikipedia:id/search_container")).click();
 
 //        WebElement searchField = pom.waitForElementAndClick(
 //                "id:org.wikipedia:id/search_container",
 //                "Cannot find Search field",
 //                5
 //        );
-
-        driver.findElement(By.xpath("//*[contains(@text, 'Search')]"))
-                .sendKeys("asfdasfdasfdasfasfdsadasdas");
-        if (checkSearchResults())
-            System.out.println("Works as expected");
-        else throw new Exception("Actual and expected results do not meet");
     }
 
+
     public boolean checkSearchResults() {
-        String actualResult = driver.findElement(By.xpath("//*[contains(@text, 'No results')]")).getText();
-        return actualResult.equals("No results");
+        String expectedResult = "No results found";
+        String actualResult = driver.findElement(By.xpath("//*[contains(@text, 'No results found')]")).getText();
+        return actualResult.equals(expectedResult);
 
     }
 
